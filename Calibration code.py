36f47a67b49e4a76f09543741710f7eedafc7350
Liam-M-Rooney <147630629+Liam-M-Rooney@users.noreply.github.com>
2023-10-11 16:08:26 +0100

    Add files via upload

---
+++ Calibration code.py
@@ -0,0 +1,133 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 30 11:45:58 2022
+
+@author: bwats
+
+This code creates 3 images which can be used to get the correct settings 
+when makeing the 3d image.
+
+1. is the image of the lens with a line that the intensity is read from
+
+2. is the intensity along the red line in figure 1, the peaks of this graph
+    should be marked with an x to mark them
+
+3. is a profile of the lens from the centre to the end of red line in figure 
+    1, the points on this graph corispond to the peaks in figure 2 
+    
+
+the # comments discribe what the code does
+the coments inside ""  discribe the places that the code might be adjusted
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.signal import find_peaks
+
+
+# creating a figure
+fig1, ((ax1)) = plt.subplots(1,1)
+
+
+'''
+imput the name of the IRM image you want to look at below 
+''' 
+# plotting the lens image
+img = plt.imread('35mm rep3.png')
+ax1.imshow(img)
+
+
+'''
+when the code is first run you can look at the image to find the coordinates of
+ the centre of the lens using ImageJ. 
+If using spyder to look at the code the figures can be brought up in a seperate
+window by using, 
+    %matplotlib qt     and    %matplotlib inline (to reverse)
+in the IPython console window. There will be the coordinates of you mouse 
+in the bottem of the screen which can help to find the centre.
+'''
+# centre of lens
+o = np.array([1026 , 1071])
+
+
+'''
+length of the red line can be adjusted so the maximum number of fringes are
+recorded - this should be the distance in pixels from the centre of the image to the edge.
+'''
+# radius of outward line
+r = 950
+
+
+'''
+angle is adjusted to check the intencity at different places on the lens 
+(in radians) 
+'''
+# angle of outward line
+angle =0
+
+# point at the end of the line
+point = np.array([r*np.cos(angle)+o[0], r*np.sin(angle)+o[1]])
+num_points = int(abs((o[0]-point[0])+1 + abs(o[1]-point[1]+1)/2))
+
+# creating the line
+xarray = np.linspace(o[0], point[0], num_points)
+m = (point[1]-o[1])/(point[0]-o[0])
+yarray = m * (xarray - point[0]) + point[1]
+
+# plotting the line
+ax1.plot(xarray, yarray, 'r')
+ax1.plot(point[0], point[1], 'xb')
+
+# the straght line but all the points are rounded to 0 d.p.
+xarray2 = np.around(xarray, 0).astype(int)
+yarray2 = np.around(yarray, 0).astype(int)
+
+# picks the same points on the lens image which is the intensity
+intensity = img[yarray2, xarray2, 0]
+
+# distance from the centre
+r_vals = np.sqrt((xarray2-xarray2[0])**2+(yarray2-yarray[0])**2)
+
+# intensity plot
+fig2, ((ax2)) = plt.subplots(1, 1, figsize=(12, 6))
+ax2.plot(r_vals, intensity, label='intensity of pixels')
+ax2.set(title='Intensity Plot Along line',
+        xlabel='Distance From Centre (m)',
+        ylabel='Intensity (AU)')
+
+'''
+the lines below can be used to adjust which peaks are selected, i've used 
+three variables in the function
+
+height - is the minimum height that a peak can be
+distance - is the minimum distance between peaks (messured in pixels)
+prominance - is to do with a peaks height relitive to its surrounding peaks 
+'''
+# selecting the peaks
+peak, _ = find_peaks(intensity, height=(0.2), 
+                     distance=(7), prominence=(0.5)) 
+
+# plots the peaks of the intensity onto the same figure
+ax2.plot(r_vals[peak], intensity[peak], 'xk', label='intensity peaks')
+ax2.legend()
+# loop creates the values for the hights at each peak 
+N = 0
+Z = []
+lamb = 458e-9
+while N < len(peak):
+    z = (N + 1/2) * lamb/2
+    Z.append(z)
+    N +=1
+
+# creates an array of all the z values
+Z = np.array(Z)
+
+# plots the graph of the profile
+fig3, ((ax3)) = plt.subplots(1, 1, figsize=(12,8))
+ax3.plot(r_vals[peak], Z, 'o', label='Bright Fringes')
+ax3.set(title='Lens Profile',
+        xlabel='Distance Along r (pixels)',
+        ylabel='Distance in z (m)')
+ax3.legend()
+
+
---
+++ Radial Analysis code.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jul  8 10:50:12 2022
+
+@author: bwats
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+def bins(x, b_num):
+    length = (max(x))/b_num # is the width of each bin
+    bin0 = np.arange(0, max(x)+length, length)
+    bin1 = np.array([])
+    i = 0
+    while i < b_num:
+        a = x[bin0[i] < x ]
+        a = a[ a < bin0[i+1]]
+        bin1 = np.append(bin1, len(a))
+        i += 1
+    return bin1
+    
+
+
+d = np.loadtxt('35mm rep3 data')
+
+
+binl = 200
+bins = bins(d, binl)
+# bins = bins[bins>0]
+x_vals = np.linspace(0, max(d), len(bins))
+y_vals = np.linspace(0, max(bins), 100)
+
+fig1, ((ax1)) = plt.subplots(1, 1, figsize=(15, 10))
+ax1.bar(x_vals, bins, color='k',  align='edge', width=0.0008, label='Practical results')
+# plt.plot(np.mean(d)* np.ones(len(y_vals)), y_vals, '--b', 
+#           label='average radius')
+ax1.plot(0.01809 * np.ones(len(y_vals)), y_vals, '--g', 
+          label='Theoretical value')
+ax1.set(title='Measured distribution of radius of curvature over lens surface')
+ax1.legend()
+ax1.set(xlabel='Radius of Curvature (m)',
+        ylabel='Frequency of points at radius')
+
+
+fig2, ((ax2)) = plt.subplots(1, 1, figsize=(15, 10))
+
+x_vals2 = np.linspace(0, len(d), len(d))
+ax2.plot(x_vals2, d, '.k', label='data points')
+ax2.set(title='lens curvature across its surface', 
+        xlabel='data points from reconstructed lens', 
+        ylabel='radius at point (m)')
+ax2.plot(x_vals2, np.ones(len(x_vals2)) * 0.01809, '-.g', 
+         label='theoretical curvature')
+ax2.legend()
+
+'''
+data in d is all the recorded points on the reconstructed lens
+the way the recorded data starts at the smallest bright fringe and the smallest 
+angle. then the next point is the smallest bright fringe at a slightly larger 
+angle, once the angle has incresed to 360 degrees the next point on in the data 
+is the second smallest fringe at angle 0. this then repeats
+
+due to this the data on the left hand side of the plot is the lens curvature at 
+small r, the waveyness of the plot is due to not being perfectly circular. 
+
+'''
+D = np.where(bins==bins.max())
+curve = x_vals[D]
+print('\n Curvature of the lens UV4 is', curve, 'm')
+
